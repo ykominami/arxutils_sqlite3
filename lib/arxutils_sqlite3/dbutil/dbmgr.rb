144162d15b7e773feaf7f9f8d0ab43c7061b1011
ykominami <ykominami@gmail.com>
2022-09-07 13:51:59 +0900

    fix #7

--- lib/arxutils_sqlite3/dbutil/dbmgr.rb
+++ lib/arxutils_sqlite3/dbutil/dbmgr.rb
@@ -8,21 +8,11 @@ module Arxutils_Sqlite3
     class DbMgr
       # DB接続の状態を示す
       @ret = nil
-      # Dbinitクラスのインスタンス生成とDB接続、現在日時取得
-      def self.init(db_dir, migrate_dir, config_dir, dbconfig, env, log_fname, opts)
-        dbinit = Dbinit.new(db_dir, migrate_dir, config_dir, dbconfig, env, log_fname, opts)
-        setup(dbinit)
-      end
-
       # DB接続、現在日時取得
       def self.setup(dbinit)
-        p "DbMgr.setup 1 @ret=#{@ret}"
         unless @ret
-          p "DbMgr.setup 2"
           begin
-            p "DbMgr.setup 3"
             dbinit.setup
-            p "DbMgr.setup 4"
             @ret = DateTime.now.new_offset
           rescue StandardError => e
             pp e.class
@@ -30,7 +20,6 @@ module Arxutils_Sqlite3
             pp e.backtrace
           end
         end
-        p "DbMgr.setup 5"
 
         @ret
       end
